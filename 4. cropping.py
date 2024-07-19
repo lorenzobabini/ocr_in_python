4c9d6ddd2540e688319a16f033c07258925a8775
Lorenzo Babini <lorenzo.babini@unicatt.it>
2024-07-19 18:20:09 +0200

    Add files via upload

---
+++ 4. cropping.py
@@ -0,0 +1,44 @@
+from PIL import Image
+from pathlib import Path
+
+input_folder = Path("output/binarized")
+output_folder = Path("output/cropped")
+output_folder.mkdir(parents=True, exist_ok=True)
+
+i=1
+for img in input_folder.rglob('*.jpg'):
+
+    im = Image.open(str(img))  # Opens a image in RGB mode
+
+    width, height = im.size  # Size of the image in pixels (size of original image)
+
+    # Setting the points for the first cropped image (in this case we crop the first half on the left)
+    ## if the split of the two pages doesn't perfectly fall in the middle of the images, you can adjust the parameters (for example: right = width / 2 + 30)
+        
+    left = 0
+    top = 0
+    right = width / 1.8
+    bottom = height-150
+
+
+    # cropping and saving
+        
+    im_cropped_left = im.crop((left, top, right, bottom)).save(str(output_folder) + '/' + "image_cropped_" + str(i * 2).zfill(3) + '.jpg' )
+
+
+    # Setting the points for the second cropped image (in this case we crop the second half on the right)
+    left = width / 1.8
+    top = 0
+    right = width
+    bottom = height-150
+
+
+    # cropping and saving
+        
+    im_cropped_right = im.crop((left, top, right, bottom)).save(str(output_folder) + '/' + "image_cropped_" + str((i * 2) + 1).zfill(3) + '.jpg' )
+
+
+    print("image" + str(i) + "cropped successfully")
+
+    i = i+1
+
---
+++ 5. rotation.py
@@ -0,0 +1,71 @@
+import os
+import PIL
+from PIL import Image
+import numpy as np
+import scipy.ndimage
+from pathlib import Path
+
+def find_score(arr, angle):
+    """Determine score for a given rotation angle.
+    """
+    data = scipy.ndimage.rotate(arr, angle, reshape=False, order=0)
+    hist = np.sum(data, axis=1)
+    score = np.sum((hist[1:] - hist[:-1]) ** 2)
+    return hist, score
+
+def rotation_angle(img):
+    """Determine the best angle to rotate the image to remove skew.
+    
+    Parameters:
+    img (PIL.Image.Image): Image
+    
+    Returns:
+    (int): Angle
+    """
+    wd, ht = img.size
+    pix = np.array(img.convert('1').getdata(), np.uint8)
+    bin_img = 1 - (pix.reshape((ht, wd)) / 255.0)
+   
+    delta = .5 # The number of degrees between each rotation
+    limit = 10 # The limit of the rotations in each direction
+    angles = np.arange(-limit, limit+delta, delta)
+    scores = []
+    for angle in angles:
+        hist, score = find_score(bin_img, angle)
+        scores.append(score)
+    
+    best_score = max(scores)
+    best_angle = angles[scores.index(best_score)]
+    
+    return float(best_angle)
+
+
+
+input_folder = Path("output/cropped")
+output_folder = Path("output/rotated")
+output_folder.mkdir(parents=True, exist_ok=True)
+
+i=0
+for image in input_folder.rglob('*.jpg'):
+
+# Open the image file to rotate
+    img = Image.open(image)
+
+## Use rotation_angle and find_score to determine angle
+    angle_1 = rotation_angle(img)
+    print(angle_1)
+
+### Rotating the image based on the best angle ###
+
+# Rotate the image
+# expand = True expands the image size to keep all data, filling space with white pixels
+# PIL.Image.Resampling.BICUBIC chooses BICUBIC resampling, a more accurate form of
+# resampling than the default: Nearest Neighbors
+    im1 = img.rotate(angle_1, PIL.Image.Resampling.BICUBIC, expand = True)
+ 
+# to save rotated image
+    im1.save(str(output_folder)+"/rotated_image_" + str(i).zfill(3)+".jpg")
+
+    i = i+1
+
+
