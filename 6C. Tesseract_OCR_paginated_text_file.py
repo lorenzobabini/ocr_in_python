47190f13957215987034fecf7e4604b7ee7aa17c
Lorenzo Babini <lorenzo.babini@unicatt.it>
2024-07-24 11:46:02 +0200

    Update 6C. Tesseract_OCR_paginated_text_file.py

--- 6C. Tesseract_OCR_paginated_text_file.py
+++ 6C. Tesseract_OCR_paginated_text_file.py
@@ -7,8 +7,7 @@ from pathlib import Path
 
 # To use Tesseract, in the Python code you must specify the path of tesseract.exe (or insert it in the system environment variables)
 ## that in the default installation options is normally saved in C:/Users/AppData/Local/Tesseract-OCR/tesseract.exe
-
-pytesseract.pytesseract.tesseract_cmd = '' #insert yout Tesseract path
+#pytesseract.pytesseract.tesseract_cmd = '' #insert yout Tesseract path
 
 # Here are three ways to customize the settings (engine modes, page segmentation modes or boths)
 custom_oem_config = r'--oem 3'
