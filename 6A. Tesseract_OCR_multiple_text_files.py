473dee96fdcf19825a21a30f39d2c0b3e0ac3757
Lorenzo Babini <lorenzo.babini@unicatt.it>
2024-07-24 11:22:56 +0200

    Add files via upload

---
+++ 6A. Tesseract_OCR_multiple_text_files.py
@@ -0,0 +1,36 @@
+# download and install Tesseract in your machine for your specific operating system (https://tesseract-ocr.github.io/tessdoc/Installation.html)
+# install Pytesseract with pip using cmd ("pip install pytesseract")
+
+from PIL import Image
+import pytesseract
+from pathlib import Path
+
+# To use Tesseract, in the Python code you must specify the path of tesseract.exe (or insert it in the system environment variables: https://www.architectryan.com/2018/03/17/add-to-the-path-on-windows-10/)
+## that in the default installation options is normally saved in C:/Users/AppData/Local/Tesseract-OCR/tesseract.exe
+
+pytesseract.pytesseract.tesseract_cmd = 'tools/Tesseract-OCR/tesseract.exe'
+
+# Here are three ways to customize the settings (engine modes, page segmentation modes or boths)
+custom_oem_config = r'--oem 3'
+custom_psm_config = r'--psm 3'
+custom_oem_psm_config = r'--oem 3 --psm 3'
+
+
+input_folder = Path('output/rotated')
+output_folder = Path("output/texts")
+output_folder.mkdir(parents=True, exist_ok=True)
+
+i = 1
+for img in input_folder.iterdir():
+    if img.suffix == '.jpg':
+        
+        with open (img, 'rb') as f:
+            file= Image.open(f)
+            ocrText= pytesseract.image_to_string(file, lang="ita", config=custom_oem_psm_config)
+
+        with open (str(output_folder) + "/page_" + str(i).zfill(3) +".txt", 'w', encoding='utf-8') as text:
+            text.write(ocrText)
+
+        print("page n_" + str(i) + " OCRed")
+
+        i = i+1
